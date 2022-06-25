41877382536f37c78c601511a71dbe78e45b5f0f
Alexander Levin <alevin.ru@gmail.com>
2022-06-25 11:56:52 +0300

    PropInputOptions with save

--- src/components/props/PropInputOptions.vue
+++ src/components/props/PropInputOptions.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 
 .prop-input-options
+
   el-select(
     allow-create
     filterable
@@ -9,20 +10,54 @@
     :size="size"
     value-key="id"
   )
+    el-option(v-for="{ id, name } in options" :label="name" :value="{id}")
+
+  el-button(
+    v-if="isNew" size="mini" icon="el-icon-plus" @click="onAdd" circle type="success"
   )
-    el-option(label="Test" value="test")
 
 </template>
 
 <script>
 import PropInput from '@/components/props/PropInputBase';
+import PropOption from '@/models/PropOption';
 
 export default {
   name: 'PropInputOptions',
   mixins: [PropInput],
+  computed: {
+    propId() {
+      return this.prop.id;
+    },
+    options() {
+      const { propId = null } = this;
+      return PropOption.reactiveFilter({ propId });
+    },
+    isNew() {
+      return this.value && !this.value.id;
+    },
+  },
+  methods: {
+    onAdd() {
+      this.$saveWithLoading(async () => {
+        const { value: name, propId } = this;
+        this.value = await PropOption.create({ name, propId });
+      });
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
+@import "../../styles/variables";
+
+.prop-input-options {
+  display: flex;
+  align-items: center;
+}
+
+.el-button {
+  margin-left: $margin-right;
+}
 
 </style>
