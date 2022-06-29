594a73eb3e5bbf64a8b41b05051f6ca5919e2e10
Alexander Levin <alevin.ru@gmail.com>
2022-06-29 16:44:47 +0300

    Update dataSync.js

--- src/services/dataSync.js
+++ src/services/dataSync.js
@@ -1,8 +1,10 @@
 import ArticleProp from '@/models/ArticleProp';
 import PropOption from '@/models/PropOption';
+import Article from '@/models/Article';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function initData() {
   await ArticleProp.findAll();
   await PropOption.findAll();
+  await Article.findAll();
 }
