0c5901fce899344fd4c0f91b3994226bdacff66c
Alexander Levin <alevin.ru@gmail.com>
2022-06-29 10:57:50 +0300

    Update ArticleMatcher.vue

--- src/components/catalogue/ArticleMatcher.vue
+++ src/components/catalogue/ArticleMatcher.vue
@@ -4,12 +4,17 @@
 
   property-matcher(:title="title" v-model="filters")
 
-  .name
-    label Name:
-    strong {{ this.compoundName }}
-
-  .articles(v-if="filteredArticles.length")
-    .article(v-for="article in filteredArticles" :key="article.id")
+  .list-group.articles(v-if="this.compoundName")
+    .list-group-header
+      .compound-name
+        .label(size="mini" v-t="'fields.name'")
+        strong {{ this.compoundName }}
+      .create(v-if="allowCreate")
+        el-button.create(type="success" v-t="'createArticle'" @click="onCreateClick" size="mini")
+    .list-group-item.article(v-for="article in filteredArticles" :key="article.id")
+      .name {{ article.name }}
+    .list-group-item.no-articles(v-if="!filteredArticles.length")
+      .no-matches(v-t="'noMatches'")
 
 </template>
 <script>
@@ -17,6 +22,24 @@ import PropertyMatcher from '@/components/props/PropertyMatcher.vue';
 import Article from '@/models/Article';
 import lowerFirst from 'lodash/lowerFirst';
 import upperFirst from 'lodash/upperFirst';
+import fpPick from 'lodash/fp/pick';
+import keyBy from 'lodash/keyBy';
+import { createNamespacedHelpers } from 'vuex';
+
+import { ARTICLE_FILTERS } from '@/store/inv/getters';
+import { SET_ARTICLE_FILTERS } from '@/store/inv/mutations';
+import { VALUE_TYPES } from '@/models/ArticleProp';
+
+const { mapGetters, mapMutations } = createNamespacedHelpers('inv');
+
+const PICK_PROPS = fpPick([
+  'propId',
+  'optionId',
+  'boolValue',
+  'stringValue',
+  'numberValue',
+  'type',
+]);
 
 export default {
   name: 'ArticleMatcher',
@@ -24,8 +47,16 @@ export default {
     title: String,
   },
   computed: {
+    filters: {
+      ...mapGetters({ get: ARTICLE_FILTERS }),
+      ...mapMutations({ set: SET_ARTICLE_FILTERS }),
+    },
     filteredArticles() {
-      return Article.reactiveFilter(this.arrayToFilters());
+      return this.filters.length ? Article.reactiveFilter(this.arrayToFilters()) : [];
+    },
+    allowCreate() {
+      const { compoundName, filteredArticles } = this;
+      return compoundName && !filteredArticles.find(a => a.name === compoundName);
     },
     compoundName() {
       const res = this.filters
@@ -48,30 +79,92 @@ export default {
   },
   methods: {
     async createArticle() {
-      await Article.create({ props: this.filters });
+      const props = this.filters.map(PICK_PROPS);
+      await Article.create({
+        props,
+        name: this.compoundName,
+      });
     },
     arrayToFilters() {
-      return ({ props }) => !this.filters.find(filter => props[filter.propId]);
+      return ({ props }) => {
+        const byId = keyBy(props, 'propId');
+        const notMatching = this.filters.find(filter => {
+          const prop = byId[filter.propId];
+          if (!prop) {
+            return true;
+          }
+          const valueName = VALUE_TYPES[filter.prop.type];
+          const filterValue = filter[valueName];
+          return filterValue !== null
+            && filterValue !== undefined
+            && filterValue !== prop[valueName];
+        });
+        return !notMatching;
+      };
+    },
+    onCreateClick() {
+      this.$saveWithLoading(this.createArticle);
     },
   },
   data() {
     return {
-      filters: [],
+      // filters: [],
     };
   },
   components: { PropertyMatcher },
+  i18n: {
+    messages: {
+      en: {
+        createArticle: 'Create',
+        noMatches: 'No matching articles',
+      },
+      lt: {
+        createArticle: 'Kurti',
+        noMatches: 'Nėra tinkamos nomenklatūros',
+      },
+      ru: {
+        createArticle: 'Создать',
+        noMatches: 'Нет подходящей номенклатуры',
+      },
+    },
+  },
 };
 
 </script>
 <style scoped lang="scss">
 @import "../../styles/variables";
 
-.name {
+.articles {
   margin: $margin-top auto;
 }
 
-label {
+.list-group {
+  max-width: 350px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.list-group-header {
+  display: flex;
+}
+
+.compound-name {
+  flex: 1;
+  align-items: center;
+}
+
+.el-button.create {
+  padding: 3px 6px;
+}
+
+.label {
+  font-size: 10px;
+  padding: 0 $padding;
+  display: inline;
+  border: $list-cell-borders;
+  border-radius: $padding;
   margin-right: $padding;
+  font-weight: normal;
 }
 
 </style>
