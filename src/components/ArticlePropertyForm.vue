48bc24675cb423fc8d17aa4f1f1a1c95af3b15bf
Alexander Levin <alevin.ru@gmail.com>
2022-06-29 14:49:57 +0300

    dataTypes localized

--- src/components/ArticlePropertyForm.vue
+++ src/components/ArticlePropertyForm.vue
@@ -13,16 +13,17 @@
 
     el-form-item(:label="$t('fields.type')" prop="type")
       el-select(v-model="model.type")
-        el-option(:label="$t('string')" value="string")
-        el-option(:label="$t('options')" value="options")
-        el-option(:label="$t('boolean')" value="boolean")
-        el-option(:label="$t('number')" value="number")
+        el-option(
+          v-for="type in types" :key="type"
+          :label="$t(`dataTypes.${type}`)" :value="type"
+        )
 
     el-form-item(:label="$t('fields.required')")
       el-switch(v-model="model.isRequired")
 
 </template>
 <script>
+import { VALUE_TYPES } from '@/models/ArticleProp';
 
 export default {
   name: 'ArticlePropertyForm',
@@ -33,33 +34,14 @@ export default {
     },
   },
   computed: {
+    types() {
+      return Object.keys(VALUE_TYPES);
+    },
     rules() {
       return {
         ...this.$requiredRule('name'),
         ...this.$requiredRule('type'),
       };
-    },
-  },
-  i18n: {
-    messages: {
-      en: {
-        string: 'String',
-        options: 'Options',
-        boolean: 'Checkbox',
-        number: 'Number',
-      },
-      ru: {
-        string: 'Строка',
-        options: 'Выбор из списка',
-        boolean: 'Есть/Нет',
-        number: 'Число',
-      },
-      lt: {
-        string: 'Eilutė',
-        options: 'Pasirinkimas iš sąrašo',
-        boolean: 'Taip/Ne',
-        number: 'Skaičius',
-      },
     },
   },
 };
