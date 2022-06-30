011d6758abfa807ffc46d17633251b7d903b7b61
Alexander Levin <alevin.ru@gmail.com>
2022-06-30 11:24:19 +0300

    ArticlePropertyForm prefix and suffix

--- src/components/ArticlePropertyForm.vue
+++ src/components/ArticlePropertyForm.vue
@@ -11,12 +11,24 @@
     el-form-item(:label="$t('fields.name')" prop="name")
       el-input(v-model="model.name")
 
+    el-form-item(:label="$t('fields.prefix')" prop="prefix")
+      el-input(v-model="model.prefix")
+
+    el-form-item(:label="$t('fields.suffix')" prop="suffix")
+      el-input(v-model="model.suffix")
+
     el-form-item(:label="$t('fields.type')" prop="type")
       el-select(v-model="model.type")
         el-option(
           v-for="type in types" :key="type"
           :label="$t(`dataTypes.${type}`)" :value="type"
         )
+
+    el-form-item(
+      :label="$t('fields.inverse')" prop="inverse"
+      v-if="model.type === 'boolean'"
+    )
+      el-input(v-model="model.inverse")
 
     el-form-item(:label="$t('fields.required')")
       el-switch(v-model="model.isRequired")
