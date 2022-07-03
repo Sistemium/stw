a0df39ee97e1920a8670c049f5ad923e3105a13a
Alexander Levin <alevin.ru@gmail.com>
2022-07-03 10:45:19 +0300

    Update dataSync.js

--- src/services/dataSync.js
+++ src/services/dataSync.js
@@ -1,9 +1,13 @@
+// import log from 'sistemium-debug';
 import ArticleProp from '@/models/ArticleProp';
 import PropOption from '@/models/PropOption';
 import Article from '@/models/Article';
 
+// const { debug } = log('initData');
+
 // eslint-disable-next-line import/prefer-default-export
 export async function initData() {
+  // debug('start');
   await ArticleProp.findAll();
   await PropOption.findAll();
   await Article.findAll();
