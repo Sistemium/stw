f7d341c207e5a05ab0cbfc1b1ccfac45261e3400
Alexander Levin <alevin.ru@gmail.com>
2022-07-09 19:49:05 +0300

    Create ArticleList.vue

---
+++ src/components/catalogue/ArticleList.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+
+.article-list.list-group
+  slot(name="header")
+  .list-group-item(
+    v-for="article in articles" :key="article.id"
+    @click="$emit('click', article)"
+  )
+    .name {{ article.name }}
+  slot(name="footer")
+
+</template>
+<script>
+
+export default {
+  name: 'ArticleList',
+  props: {
+    articles: Array,
+  },
+};
+
+</script>
+<style scoped lang="scss">
+
+</style>
