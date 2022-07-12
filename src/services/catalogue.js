494011055e0decf66c6c38f81b2c5590e65cd1c6
Alexander Levin <alevin.ru@gmail.com>
2022-07-12 14:25:15 +0300

    Create catalogue.js

---
+++ src/services/catalogue.js
@@ -0,0 +1,29 @@
+import lowerFirst from 'lodash/lowerFirst';
+import upperFirst from 'lodash/upperFirst';
+import trim from 'lodash/trim';
+import map from 'lodash/map';
+import ArticleProp from '@/models/ArticleProp';
+
+// eslint-disable-next-line import/prefer-default-export
+export function compoundName(filters) {
+  const res = map(filters, filter => {
+    const { stringValue, numberValue } = filter;
+    const simple = stringValue || numberValue;
+    const prop = filter.prop || ArticleProp.getByID(filter.propId) || {};
+    if (simple) {
+      const { prefix, suffix } = prop;
+      return [prefix, simple, suffix]
+        .filter(a => a)
+        .join('');
+    }
+    const { boolValue } = filter;
+    if (boolValue !== undefined && prop) {
+      return boolValue ? prop.name : prop.inverse;
+    }
+    return '';
+  })
+    .filter(x => x)
+    .map(lowerFirst)
+    .join(' ');
+  return upperFirst(trim(res));
+}
