fbf8a7b51ab9f43a6738aa989551856ae77fcf23
Alexander Levin <alevin.ru@gmail.com>
2022-07-15 14:23:59 +0300

    Create ArticleView.vue

---
+++ src/components/catalogue/ArticleView.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+
+.article-view(v-if="article")
+  .name {{ article.name }}
+
+</template>
+<script>
+import Article from '@/models/Article';
+
+export default {
+  name: 'ArticleView',
+  props: {
+    articleId: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    article() {
+      return Article.reactiveGet(this.articleId);
+    },
+  },
+};
+
+</script>
+<style scoped lang="scss">
+
+</style>
