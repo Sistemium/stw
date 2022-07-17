fdfb04a4047a0823889dd3fd73952db867332848
Alexander Levin <alevin.ru@gmail.com>
2022-07-17 19:10:26 +0300

    $requiredRule(fieldOrArray)

--- src/lib/validations.js
+++ src/lib/validations.js
@@ -3,13 +3,18 @@ import Vue from 'vue';
 Vue.mixin({
 
   methods: {
-    $requiredRule(field) {
-      return {
-        [field]: [{
+    $requiredRule(fieldOrArray) {
+      const res = {};
+      const fields = Array.isArray(fieldOrArray) ? fieldOrArray : [fieldOrArray];
+      fields.forEach(field => {
+        const [, concept] = field.match(/(.+)Id$/) || [];
+        const label = `${concept ? 'concepts' : 'fields'}.${concept || field}`;
+        res[field] = [{
           required: true,
-          message: this.$t('validation.required', [this.$t(`fields.${field}`)]),
-        }],
-      };
+          message: this.$t('validation.required', [this.$t(label)]),
+        }];
+      });
+      return res;
     },
     $tAction(action, name) {
       return this.$t(`actions.${action}`, [this.$t(`accusative.${name}`)]);
