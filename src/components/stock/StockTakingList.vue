4d7d5d5760450ba14a6c7d26485bd9f410cd58e0
Alexander Levin <alevin.ru@gmail.com>
2022-07-17 19:11:38 +0300

    Create StockTakingList.vue

---
+++ src/components/stock/StockTakingList.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+
+.stock-taking-list.list-group
+  stock-taking-list-item-view(
+    v-for="item in items" :key="item.id"
+    :stock-taking-item="item"
+    @click="$emit('click', item)"
+  )
+
+</template>
+<script>
+
+import StockTakingListItemView from '@/components/stock/StockTakingListItemView.vue';
+
+export default {
+  name: 'StockTakingList',
+  components: { StockTakingListItemView },
+  props: {
+    items: Array,
+  },
+  methods: {},
+};
+
+</script>
+<style scoped lang="scss">
+
+</style>
