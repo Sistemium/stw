6e6c3f7f6570832212035ca2d94ffc4aa63c57a9
Alexander Levin <alevin.ru@gmail.com>
2022-07-17 14:44:17 +0300

    Create PageTitle.vue

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+
+h1.page-title(v-t="title")
+
+</template>
+<script>
+
+export default {
+  name: 'PageTitle',
+  props: {
+    title: String,
+  },
+};
+
+</script>
+<style scoped lang="scss">
+@import "../styles/variables";
+@include responsive-only(xxs) {
+  h1 {
+    display: none;
+  }
+}
+</style>
