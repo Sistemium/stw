6801037997d1462ad0df2d77a3b68661e1716aeb
Alexander Levin <alevin.ru@gmail.com>
2022-07-22 15:46:13 +0300

    Create BarcodeScanner.js

---
+++ src/components/BarcodeScanner/BarcodeScanner.js
@@ -0,0 +1,39 @@
+import { createNamespacedHelpers } from 'vuex';
+import * as g from '@/store/inv/getters';
+import * as m from '@/store/inv/mutations';
+
+const { mapGetters, mapMutations } = createNamespacedHelpers('inv');
+
+export default {
+  name: 'BarcodeScanner',
+  props: {
+    emitOnCreate: Boolean,
+  },
+  render() {
+  },
+  computed: {
+    ...mapGetters({
+      scannedBarCode: g.SCANNED_BARCODE,
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      clearBarcode: m.SET_SCANNED_BARCODE,
+    }),
+  },
+  watch: {
+    scannedBarCode(barCode) {
+      this.$emit('input', barCode);
+      if (barCode) {
+        this.$emit('scan', barCode);
+      } else {
+        this.$emit('clear');
+      }
+    },
+  },
+  created() {
+    if (this.scannedBarCode) {
+      this.$emit('input', this.scannedBarCode);
+    }
+  },
+};
