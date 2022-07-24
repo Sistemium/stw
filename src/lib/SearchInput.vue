6f7a4c28333a1601fcec129b6f50f1c336d06faa
Alexander Levin <alevin.ru@gmail.com>
2022-07-24 12:37:43 +0300

    SearchInput

---
+++ src/lib/SearchInput.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+
+el-input.search-input(
+  prefix-icon="el-icon-search"
+  v-model="searchText"
+  :clearable="true"
+  :placeholder="placeholder"
+  :size="size"
+  :disabled="disabled"
+)
+
+</template>
+<script>
+
+import debounce from 'lodash/debounce';
+
+export default {
+
+  name: 'SearchInput',
+
+  props: {
+    disabled: Boolean,
+    size: {
+      type: String,
+    },
+    value: String,
+    placeholder: {
+      default() {
+        return this.$t('search');
+      },
+      type: String,
+    },
+    debounce: {
+      type: Number,
+      default: 500,
+    },
+  },
+
+  data() {
+    return { searchText: this.value };
+  },
+
+  created() {
+    this.$watch('searchText', debounce(value => this.$emit('input', value), this.debounce));
+  },
+
+};
+
+</script>
+<style scoped lang="scss">
+
+@import "../styles/variables";
+
+.el-input {
+  width: unset;
+}
+
+</style>
