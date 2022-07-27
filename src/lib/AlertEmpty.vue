329e18219a74cea4bf25ed4e00bc53ca2f7c0f61
Alexander Levin <alevin.ru@gmail.com>
2022-07-27 14:47:22 +0300

    Create AlertEmpty.vue

---
+++ src/lib/AlertEmpty.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+
+el-alert.alert-empty(
+  :title="title"
+  :closable="false"
+)
+  el-button(
+    v-if="buttonText"
+    @click="$emit('click')"
+    type="primary"
+    :plain="true"
+  ) {{ buttonText }}
+
+</template>
+<script>
+
+export default {
+  name: 'AlertEmpty',
+  props: {
+    title: String,
+    buttonText: String,
+  },
+};
+
+</script>
+<style scoped lang="scss">
+.el-alert ::v-deep .el-alert__content {
+  flex: 1;
+
+  .el-alert__description {
+    text-align: right;
+  }
+}
+</style>
