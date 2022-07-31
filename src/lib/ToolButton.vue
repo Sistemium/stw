004868e4209081f22817402d38c5b4f208d128be
Alexander Levin <alevin.ru@gmail.com>
2022-07-31 09:07:17 +0300

    ToolButton with default tool

--- src/lib/ToolButton.vue
+++ src/lib/ToolButton.vue
@@ -24,10 +24,10 @@ export default {
     tool: {
       type: String,
       required: true,
-      validator(value) {
-        return Object.keys(ICONS)
-          .includes(value);
-      },
+      // validator(value) {
+      //   return Object.keys(ICONS)
+      //     .includes(value);
+      // },
     },
     busy: {
       type: Boolean,
@@ -48,7 +48,7 @@ export default {
       if (this.busy && tool === 'refresh') {
         return ICONS.loading;
       }
-      return ICONS[tool];
+      return ICONS[tool] || `el-icon-${tool}`;
     },
   },
 };
