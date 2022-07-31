45b490054919bcdd3beb62e3110bf9a812a498cc
Alexander Levin <alevin.ru@gmail.com>
2022-07-31 11:38:32 +0300

    StockWithdrawingPage with tabs

--- src/views/StockWithdrawingPage.vue
+++ src/views/StockWithdrawingPage.vue
@@ -1,27 +1,36 @@
 <template lang="pug">
 
 .stock-withdrawing
-  //page-title()
-  .buttons
-    tool-button(tool="add" @click="onAddItem")
-  resize(:padding="20")
-    stock-withdrawing-item-list(
-      :items="stockWithdrawingItems"
-      @click="onItemClick"
-      v-if="stockWithdrawingItems.length"
-    )
-    alert-empty(v-else @click="onAddItem" :button-text="$tAction('add', 'position')")
+
+  el-tabs
+    el-tab-pane(:lazy="true" :label="$t('concepts.items')")
+      .buttons
+        tool-button(tool="add" @click="onAddItem")
+      resize(:padding="20")
+        stock-withdrawing-item-list(
+          :items="stockWithdrawingItems"
+          @click="onItemClick"
+          v-if="stockWithdrawingItems.length"
+        )
+        alert-empty(v-else @click="onAddItem" :button-text="$tAction('add', 'position')")
+    el-tab-pane(:lazy="true" :label="$t('concepts.settings')")
+      stock-withdrawing-edit(
+        :is-drawer="false"
+        :stock-withdrawing-id="stockWithdrawingId"
+        @closed="onEditClose"
+      )
   router-view
 
 </template>
 <script>
 import StockWithdrawingItem from '@/models/StockWithdrawingItem';
 import StockWithdrawingItemList from '@/components/out/StockWithdrawingItemList.vue';
+import StockWithdrawingEdit from '@/components/out/StockWithdrawingEdit.vue';
 import pageMixin from '@/lib/pageMixin';
 
 export default {
   name: 'StockWithdrawingPage',
-  components: { StockWithdrawingItemList },
+  components: { StockWithdrawingEdit, StockWithdrawingItemList },
   mixins: [pageMixin],
   props: {
     stockWithdrawingId: String,
@@ -53,12 +62,26 @@ export default {
         },
       });
     },
+    onEditClose(record) {
+      if (!record) {
+        this.$router.replace({ name: this.from.name });
+      }
+    },
   },
 };
 
 </script>
 <style scoped lang="scss">
+@import "../styles/variables";
+
 .stock-withdrawing {
   flex: 1;
+}
+
+.stock-withdrawing-edit {
+  text-align: left;
+  @include responsive-only(xxs) {
+    text-align: right;
+  }
 }
 </style>
