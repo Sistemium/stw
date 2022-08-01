7714e978aa118bfdc34e0ff2dfedf4f7f5e21a82
Alexander Levin <alevin.ru@gmail.com>
2022-08-01 15:40:38 +0300

    LegalEntityEdit

---
+++ src/components/contacts/LegalEntityEdit.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+
+drawer-edit.legal-entity-edit(
+  :title="$tGen('editing', 'legalEntity')"
+  :save-fn="saveFn"
+  :destroy-fn="destroyFn"
+  :model-origin="modelOrigin"
+  :from="from"
+  :deletable="true"
+  :is-drawer="isDrawer"
+)
+  template(v-slot="{ model }")
+    legal-entity-form(:model="model")
+
+</template>
+<script>
+import drawerEditMixin from '@/lib/drawerEditMixin';
+import LegalEntity from '@/models/LegalEntity';
+import LegalEntityForm from '@/components/contacts/LegalEntityForm.vue';
+
+export default {
+  name: 'LegalEntityEdit',
+  components: { LegalEntityForm },
+  mixins: [drawerEditMixin],
+  props: {
+    legalEntityId: String,
+  },
+  computed: {
+    stockWithdrawing() {
+      return LegalEntity.reactiveGet(this.legalEntityId);
+    },
+    modelOrigin() {
+      const { legalEntityId } = this;
+      return legalEntityId ? LegalEntity.reactiveGet(legalEntityId)
+        : {
+          name: null,
+          code: null,
+          vatCode: null,
+          address: null,
+        };
+    },
+    editable() {
+      const { processing } = this.stockWithdrawing || {};
+      return processing === 'progress';
+    },
+  },
+  methods: {
+    saveFn(props) {
+      return LegalEntity.createOne(props);
+    },
+    destroyFn(id) {
+      return LegalEntity.destroy(id);
+    },
+  },
+};
+
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/contacts/LegalEntityForm.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+
+el-form.legal-entity-form(
+  :model="model"
+  ref="form"
+  :rules="rules"
+  :disabled="disabled"
+)
+
+  el-form-item(:label="$t('fields.name')" prop="name")
+    el-input(v-model="model.name")
+
+  el-form-item(:label="$t('fields.regCode')" prop="code")
+    el-input(v-model="model.code")
+
+  el-form-item(:label="$t('fields.vatCode')" prop="vatCode")
+    el-input(v-model="model.vatCode")
+
+  el-form-item(:label="$t('fields.address')" prop="address")
+    el-input(v-model="model.address")
+
+</template>
+<script>
+
+export default {
+  name: 'LegalEntityForm',
+  props: {
+    model: Object,
+    disabled: Boolean,
+  },
+  computed: {
+    rules() {
+      return this.$requiredRule(['name']);
+    },
+  },
+};
+
+</script>
+<style scoped lang="scss">
+
+</style>
