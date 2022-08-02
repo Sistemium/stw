c19a9f4eb9ce7c393616528d11f5e72955d1f52a
Alexander Levin <alevin.ru@gmail.com>
2022-08-02 16:43:04 +0300

    Create WorkflowTransitions.vue

---
+++ src/lib/WorkflowTransitions.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+
+.workflow-transitions
+  el-button(
+    :disabled="disabled"
+    v-for="option in options"
+    :key="options.processing"
+    @click="onCommand(option.to)"
+    :size="size"
+  ) {{ $t(option.label) }}
+
+</template>
+<script>
+
+import workflowMixin from '@/lib/workflowMixin';
+
+export default {
+  name: 'WorkflowTransitions',
+  props: {
+    workflow: Object,
+    value: String,
+  },
+  mixins: [workflowMixin],
+};
+
+</script>
+<style scoped lang="scss">
+
+</style>
