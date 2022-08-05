82680e51776aec6ab43f607fa4fecea7ba9130fd
Alexander Levin <alevin.ru@gmail.com>
2022-08-05 16:46:40 +0300

    PrependSelect with el-select

--- src/lib/PrependSelect.vue
+++ src/lib/PrependSelect.vue
@@ -1,52 +1,23 @@
 <template lang="pug">
 
-.prepend-select
-  .prepend
-    el-button(:icon="buttonIcon" @click="$emit('buttonClick')")
-  slot
+button-prepend.prepend-select(@buttonClick="$emit('buttonClick')")
+  el-select(:value="value" @input="v => $emit('input', v)")
+    slot
 
 </template>
 <script>
 
+import ButtonPrepend from '@/lib/ButtonPrepend.vue';
+
 export default {
   name: 'PrependSelect',
+  components: { ButtonPrepend },
   props: {
-    buttonIcon: {
-      type: String,
-      default: 'el-icon-plus',
-    },
+    value: String,
   },
 };
 
 </script>
 <style scoped lang="scss">
-
-.prepend-select {
-
-  display: flex;
-  width: 100%;
-  align-items: flex-start;
-
-  ::v-deep input {
-    border-bottom-left-radius: 0;
-    border-top-left-radius: 0;
-  }
-
-  .prepend {
-    border: 1px solid #DCDFE6;
-    border-right: none;
-    border-radius: 4px 0 0 4px;
-    line-height: 1;
-    & + *{
-      flex: 1;
-    }
-  }
-
-  .el-button {
-    line-height: 10px;
-    border: transparent;
-    background-color: #F5F7FA;
-  }
-}
 
 </style>
