069234604e53357749ac5d17291197a08c481988
Alexander Levin <alevin.ru@gmail.com>
2022-08-11 08:00:54 +0300

    Update stockOperationsRoute.js

--- src/router/stockOperationsRoute.js
+++ src/router/stockOperationsRoute.js
@@ -47,6 +47,9 @@ export default function stockOperationsRoute(options) {
             createRoute: `${operationName}ItemCreate`,
             stockOperationId,
             ...commonProps,
+            from: {
+              name: rootState,
+            },
           };
         },
         children: itemRouteHelper(
