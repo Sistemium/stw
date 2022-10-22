bff577c431c723f86a5a0f0d501cddc4b5bff915
Alexander Levin <alevin.ru@gmail.com>
2022-10-22 18:12:54 +0300

    RecipeForm with MaterialsForm

--- src/components/production/RecipeForm.vue
+++ src/components/production/RecipeForm.vue
@@ -9,32 +9,16 @@
     el-form-item(:label="$t('fields.name')" prop="name")
       el-input(v-model="model.name")
 
-    //template(v-if="model.materials.length")
-    //  tool-button(tool="add" @click="onAddMaterial")
-
-    .materials
-      .material(
-        v-for="(material, idx) in model.materials"
-        :key="material.id"
-      )
-        .buttons
-          .idx
-            el-badge(type="info" :value="idx +1")
-          el-button(type="text" @click="removeMaterial(material)") {{ $t('delete') }}
-        recipe-material-form(:model="material")
-    .buttons
-      el-button(type="text" @click="onAddMaterial") {{ $tAction('add', 'material') }}
+    materials-form(:materials="model.materials")
 
 </template>
 <script>
 
-import RecipeMaterialForm from '@/components/production/RecipeMaterialForm.vue';
-import { v4 } from 'uuid';
-/* eslint-disable vue/no-mutating-props */
+import MaterialsForm from '@/components/production/MaterialsForm.vue';
 
 export default {
   name: 'RecipeForm',
-  components: { RecipeMaterialForm },
+  components: { MaterialsForm },
   props: {
     model: Object,
   },
@@ -43,33 +27,9 @@ export default {
       return this.$requiredRule('name');
     },
   },
-  methods: {
-    removeMaterial(material) {
-      const { materials } = this.model;
-      const idx = materials.indexOf(material);
-      if (idx < 0) {
-        return;
-      }
-      materials.splice(idx, 1);
-    },
-    onAddMaterial() {
-      this.model.materials.push({
-        id: v4(),
-        articleId: null,
-        measureId: null,
-        measureUnitId: null,
-        units: null,
-      });
-    },
-  },
 };
 
 </script>
 <style scoped lang="scss">
-.buttons + div {
-  margin-top: 0;
-}
-.idx {
-  flex: 1;
-}
+
 </style>
