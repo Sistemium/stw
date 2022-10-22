f7484058a9f64e58bd9e871100ef27f9f8b81f56
Alexander Levin <alevin.ru@gmail.com>
2022-10-22 18:13:05 +0300

    StockWithdrawingProductForm with materials

--- src/components/production/StockWithdrawingProductForm.vue
+++ src/components/production/StockWithdrawingProductForm.vue
@@ -6,24 +6,38 @@ el-form.stock-withdrawing-product-form(
   :rules="rules"
 )
   el-form-item(prop="recipeId" :label="$t('concepts.recipe')")
-    recipe-select(v-model="model.recipeId")
+    recipe-select(v-model="model.recipeId" @change="onRecipeChange")
   el-form-item(prop="units" :label="$t('fields.units')")
     el-input-number(v-model="model.units" :min="1")
+  materials-form(:materials="model.materials" v-if="model.recipeId")
 
 </template>
 <script>
 
+import Recipe from '@/models/Recipe';
 import RecipeSelect from '@/components/select/RecipeSelect.vue';
+import MaterialsForm from '@/components/production/MaterialsForm.vue';
+import cloneDeep from 'lodash/cloneDeep';
 
 export default {
   name: 'StockWithdrawingProductForm',
-  components: { RecipeSelect },
+  components: { MaterialsForm, RecipeSelect },
   props: {
     model: Object,
   },
   computed: {
     rules() {
       return {};
+    },
+    // recipe() {
+    //   return Recipe.reactiveGet(this.model.recipeId);
+    // },
+  },
+  methods: {
+    onRecipeChange(recipeId) {
+      const recipe = Recipe.reactiveGet(recipeId);
+      // eslint-disable-next-line vue/no-mutating-props
+      this.model.materials = cloneDeep(recipe.materials);
     },
   },
 };
@@ -33,6 +47,7 @@ export default {
 .recipe-select {
   width: 100%;
 }
+
 .el-form-item {
   text-align: right;
 }
