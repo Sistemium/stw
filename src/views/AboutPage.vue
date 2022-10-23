6cda07742c0a984b539cb9ebb0ef07c2c579cc98
Alexander Levin <alevin.ru@gmail.com>
2022-10-23 17:10:24 +0300

    AboutPage with version

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -2,12 +2,20 @@
 
 .about
   h1 {{ $t('about') }}
+  .version v{{ version }}
 
 </template>
 <script>
 
+import { version } from '../../package.json';
+
 export default {
   name: 'AboutPage',
+  computed: {
+    version() {
+      return version;
+    },
+  },
 };
 
 </script>
