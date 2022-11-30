e5733557054cafbc323d7cf12f205edd6073d953
Alexander Levin <alevin.ru@gmail.com>
2022-11-30 10:11:48 +0200

    StockTakingsPage with storageSelect

--- src/views/StockTakingsPage.vue
+++ src/views/StockTakingsPage.vue
@@ -2,10 +2,12 @@
 
 .stock-takings-page.page
   page-title(title="menu.stockTakings")
+  .filters
+    storage-select(v-model="storageId" ref="storageSelect")
   template(v-if="showList")
     .buttons(v-if="stockTakings.length")
       tool-button(tool="add" @click="pushCreate()")
-    resize(:padding="20")
+    resize(:padding="20" v-if="storageId")
       stock-taking-list(
         :items="stockTakings"
         @click="onItemClick"
@@ -23,21 +25,39 @@
 </template>
 <script>
 
+import { createNamespacedHelpers } from 'vuex';
 import find from 'lodash/find';
 import StockTaking from '@/models/StockTaking';
 import StockTakingList from '@/components/stock/StockTakingList.vue';
 import pageMixin from '@/lib/pageMixin';
+import * as g from '@/store/inv/getters';
+import * as m from '@/store/inv/mutations';
+import storageSelectMixin from '@/components/storageSelectMixin';
+
+const { mapGetters, mapMutations } = createNamespacedHelpers('inv');
 
 export default {
   name: 'StockTakingsPage',
-  mixins: [pageMixin],
+  mixins: [pageMixin, storageSelectMixin],
   components: { StockTakingList },
   props: {
     progressRoute: String,
   },
   computed: {
+    ...mapGetters({
+      defaultStorageId: g.CURRENT_STORAGE,
+    }),
+    storageId: {
+      get() {
+        return this.defaultStorageId;
+      },
+      set(id) {
+        this.setCurrentStorageId(id);
+      },
+    },
     stockTakings() {
-      return this.$orderBy(StockTaking.reactiveFilter(), ['date'], ['desc']);
+      const { storageId } = this;
+      return this.$orderBy(StockTaking.reactiveFilter({ storageId }), ['date'], ['desc']);
     },
     showList() {
       return this.$route.name
@@ -45,6 +65,9 @@ export default {
     },
   },
   methods: {
+    ...mapMutations({
+      setCurrentStorageId: m.SET_CURRENT_STORAGE,
+    }),
     onItemClick(stockTaking, toProgress = false) {
       const { processing } = stockTaking;
       const progress = processing === 'progress' || toProgress;
@@ -60,6 +83,7 @@ export default {
       this.onItemClick(stockTaking, true);
     },
   },
+
 };
 
 </script>
