e2baa051aa63b94a2ca782ece643d514875a52ad
Alexander Levin <alevin.ru@gmail.com>
2023-04-20 21:14:01 +0300

    Update PropOptionEdit.vue

--- src/components/props/PropOptionEdit.vue
+++ src/components/props/PropOptionEdit.vue
@@ -1,64 +1,62 @@
 <template lang="pug">
 
 drawer-edit.prop-option-edit(
-  :title="$t('title')"
+  :title="t('title')"
   :save-fn="saveFn"
   :destroy-fn="destroyFn"
   :model-origin="modelOrigin"
   :from="from"
-  :drawer-style="{ top: '65px' }"
+  :drawer-style="{ top: '65px', height: 'auto' }"
   :deletable="true"
 )
   template(#default="{ model }")
-    prop-option-form(ref="form" :model="model")
+    prop-option-form(
+      ref="form"
+      :model="model"
+    )
 
 </template>
-<script>
+<script setup>
+
+import { computed, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 import DrawerEdit from '@/lib/DrawerEdit.vue';
 import PropOption from '@/models/PropOption';
 import PropOptionForm from '@/components/props/PropOptionForm.vue';
 
-export default {
-  name: 'PropOptionEdit',
-  props: {
-    propOptionId: String,
-    from: Object,
-    propId: String,
-  },
-  methods: {
-    saveFn(props) {
-      return PropOption.createOne(props);
+const props = defineProps({
+  propOptionId: String,
+  from: Object,
+  propId: String,
+});
+
+const form = ref(null);
+
+function saveFn(props) {
+  return PropOption.createOne(props);
+}
+
+function destroyFn(id) {
+  return PropOption.destroy(id);
+}
+
+const modelOrigin = computed(() => {
+  const { propOptionId, propId } = props;
+  return propOptionId ? PropOption.reactiveGet(propOptionId) : { propId };
+});
+
+const { t } = useI18n({
+  messages: {
+    en: {
+      title: 'Option editing',
     },
-    destroyFn(id) {
-      return PropOption.destroy(id);
+    ru: {
+      title: 'Редактирование варианта',
     },
-  },
-  computed: {
-    modelOrigin() {
-      const { propOptionId, propId } = this;
-      return propOptionId ? PropOption.reactiveGet(propOptionId) : { propId };
-    },
-  },
-  components: {
-    PropOptionForm,
-    DrawerEdit,
-  },
-  i18n: {
-    messages: {
-      en: {
-        title: 'Option editing',
-      },
-      ru: {
-        title: 'Редактирование варианта',
-      },
-      lt: {
-        title: 'Parinkties redagavimas',
-      },
+    lt: {
+      title: 'Parinkties redagavimas',
     },
   },
-};
+});
 
 </script>
-<style scoped lang="scss">
-
-</style>
