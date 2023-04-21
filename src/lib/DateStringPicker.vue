fc9e0e77c39e78fc5c6ffdc8d61fb5a70af091a1
Alexander Levin <alevin.ru@gmail.com>
2023-04-21 18:55:43 +0300

    Update DateStringPicker.vue

--- src/lib/DateStringPicker.vue
+++ src/lib/DateStringPicker.vue
@@ -7,32 +7,28 @@ el-date-picker.date-string-picker(
 )
 
 </template>
-<script>
+<script setup>
 
 import isDate from 'lodash/isDate';
-import dayjs from 'sistemium-dates';
+import { computed } from 'vue';
+// import dayjs from 'sistemium-dates';
 
-export default {
-  name: 'DateStringPicker',
-  props: {
-    value: String,
+const props = defineProps({
+  modelValue: String,
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const defaultTime = new Date();
+const date = computed({
+  get() {
+    const { modelValue: value } = props;
+    return value && isDate(value) ? value : new Date(value || null);
   },
-  computed: {
-    defaultTime() {
-      return dayjs(new Date())
-        .format('HH:mm:ss');
-    },
-    date: {
-      get() {
-        const { value } = this;
-        return value && isDate(value) ? value : new Date(value || null);
-      },
-      set(value) {
-        this.$emit('input', value.toJSON());
-      },
-    },
+  set(value) {
+    emit('update:modelValue', value.toJSON());
   },
-};
+});
 
 </script>
 <style scoped lang="scss">
