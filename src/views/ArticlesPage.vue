6b812fdcfa893baa43a2a3f5ad058faaeb26b7eb
Alexander Levin <alevin.ru@gmail.com>
2023-04-24 18:39:58 +0300

    Update ArticlesPage.vue

--- src/views/ArticlesPage.vue
+++ src/views/ArticlesPage.vue
@@ -7,14 +7,31 @@
 
   .buttons
     search-input(v-model="search" :disabled="!!barcode")
-    download-excel-button(:data-fn="downloadExcelData" :name="downloadExcelName")
-    transition(name="bounce")
-      el-tooltip(v-if="selectedArticle")
-        template(#content) {{ copyTip }}
-        el-button(icon="el-icon-document-copy" @click="onCopy()" circle size="mini")
-    tool-button(tool="add" @click="onAdd()")
+    download-excel-button(
+      :data-fn="downloadExcelData"
+      :name="$t('menu.articles')"
+    )
+    el-tooltip(
+      v-if="selectedArticle"
+      :content="copyTip"
+    )
+      //transition(name="bounce")
+      el-button(
+        :icon="DocumentCopy"
+        circle
+        size="small"
+        @click="onCopy()"
+      )
+    tool-button(
+      tool="add"
+      @click="onAdd()"
+    )
 
-  resize#articles-page-scroll(:padding="20" ref="resize" @resized="setHeight")
+  // ref="resizeRef"
+  resize#articles-page-scroll(
+    :padding="20"
+    @resized="setHeight"
+  )
     template(
       v-if="articles.length"
     )
@@ -23,17 +40,22 @@
         :prop-columns="tableData.propColumns"
         :size="tableSize"
         :articles="articles"
+        :height="tableHeight"
         @current-change="handleCurrentChange"
         @click="onArticleClick"
         @avatar-click="avatarClick"
-        :height="tableHeight"
       )
-    el-alert.empty(type="info" :title="$t('validation.noData')" :closable="false" v-else)
+    el-alert.empty(
+      v-else
+      type="info"
+      :title="$t('validation.noData')"
+      :closable="false"
+    )
 
   router-view
 
 </template>
-<script>
+<script setup>
 
 import each from 'lodash/each';
 import pick from 'lodash/pick';
@@ -52,171 +74,159 @@ import vssMixin from '@/components/vssMixin';
 import DownloadExcelButton from '@/lib/DownloadExcelButton.vue';
 import Resize from '@/lib/Resize.vue';
 import ToolButton from '@/lib/ToolButton.vue';
+import { computed, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import map from 'lodash/map';
+import { useRouter } from 'vue-router';
+import i18n from '@/i18n';
+import useResponsiveTables from '@/components/useResponsiveTables';
+import { DocumentCopy } from '@element-plus/icons-vue';
 
-export default {
-  name: 'ArticlesPage',
-  props: {
-    editRoute: String,
-    createRoute: String,
-    galleryRoute: String,
-  },
-  data() {
-    return {
-      barcode: null,
-      search: '',
-      tableHeight: undefined,
-      selectedArticle: null,
+const props = defineProps({
+  editRoute: String,
+  createRoute: String,
+  galleryRoute: String,
+});
+
+// const resizeRef = ref(null);
+const barcode = ref(null);
+const search = ref('');
+const tableHeight = ref(undefined);
+const selectedArticle = ref(null);
+const router = useRouter();
+
+// mixins: [
+//   scrollToCreated({ container: '#articles-page-scroll' }),
+// ],
+
+const copyTip = computed(() => {
+  const { code, name } = selectedArticle.value || {};
+  return t('copyTip', [code || name]);
+});
+
+const { showTable, tableSize } = useResponsiveTables();
+const viewComponent = computed(() => showTable.value ? ArticleTable : ArticleList);
+
+const articles = computed(() => {
+  const rowFilter = !barcode.value ? searchArticle(search.value, tableData.value.propColumns)
+    : (({ barcodes }) => barcodes && barcodes.includes(barcode));
+  return filter(tableData.value.rows, rowFilter);
+});
+
+const tableData = computed(() => {
+  const allProps = new Map();
+  const rows = map(Article.reactiveFilter(), item => {
+    const res = {
+      ...item,
     };
-  },
-  mixins: [
-    scrollToCreated({ container: '#articles-page-scroll' }),
-    vssMixin,
-  ],
-  components: {
-    ToolButton,
-    Resize,
-    DownloadExcelButton,
-    SearchInput,
-    BarcodeView,
-    PageTitle,
-    ArticleTable,
-    ArticleList,
-  },
-  computed: {
-    downloadExcelName() {
-      return this.$t('menu.articles');
-    },
-    copyTip() {
-      const { code, name } = this.selectedArticle || {};
-      return this.$t('copyTip', [code || name]);
-    },
-    viewComponent() {
-      return this.showTable ? 'article-table' : 'article-list';
-    },
-    articles() {
-      const { barcode, search, tableData } = this;
-      const rowFilter = !barcode ? searchArticle(search, tableData.propColumns)
-        : (({ barcodes }) => barcodes && barcodes.includes(barcode));
-      return filter(tableData.rows, rowFilter);
-    },
-    tableData() {
-      const allProps = new Map();
-      const rows = this.$map(Article.reactiveFilter(), item => {
-        const { props } = item;
-        const res = {
-          ...item,
-        };
-        each(props, ({ propId, stringValue, numberValue }) => {
-          allProps.set(propId, true);
-          res[propId] = stringValue || numberValue;
-        });
-        return res;
-      });
-      const propColumns = Array.from(allProps.keys())
-        .map(id => {
-          const prop = ArticleProp.reactiveGet(id);
-          if (prop.isNaming) {
-            return false;
-          }
-          return {
-            ...prop,
-            align: prop.type === 'number' ? 'right' : 'left',
-          };
-        })
-        .filter(x => x);
+    each(item.props, ({ propId, stringValue, numberValue }) => {
+      allProps.set(propId, true);
+      res[propId] = stringValue || numberValue;
+    });
+    return res;
+  });
+  const propColumns = Array.from(allProps.keys())
+    .map(id => {
+      const prop = ArticleProp.reactiveGet(id);
+      if (prop.isNaming) {
+        return false;
+      }
       return {
-        rows: orderBy(rows, 'name'),
-        propColumns: articlePropertySort(propColumns),
+        ...prop,
+        align: prop.type === 'number' ? 'right' : 'left',
       };
-    },
-    // search: {
-    //   get() {
-    //     return this.$route.query.search || '';
-    //   },
-    //   set(search) {
-    //     this.updateRouteParams({}, { search });
-    //   },
-    // },
-  },
-  i18n: {
-    messages: {
-      en: {
-        copyTip: 'Click to copy "{0}" article',
-      },
-      lt: {
-        copyTip: 'Spustelkite kad nukopijuoti "{0}" prekę',
-      },
-      ru: {
-        copyTip: 'Нажмите, чтобы скопировать номенклатуру "{0}"',
-      },
-    },
-  },
-  methods: {
-    setHeight(height) {
-      this.tableHeight = height;
-    },
-    downloadSchema() {
-      return {
-        columns: [
-          {
-            key: 'name',
-            title: this.$t('fields.name'),
-            width: 50,
-          }, {
-            key: 'code',
-            title: this.$t('fields.code'),
-            width: 25,
-          },
-          ...this.tableData.propColumns
-            .map(({ id, name }) => ({ key: id, title: name, width: 25 })),
-        ],
-      };
-    },
-    downloadExcelData() {
-      const schema = this.downloadSchema();
-      const columns = this.$map(schema.columns, 'key');
-      const data = this.articles.map(a => pick(a, columns));
-      return {
-        schema,
-        data,
-      };
-    },
-    onAdd() {
-      this.$router.push({
-        name: this.createRoute,
-      });
-    },
-    onCopy() {
-      this.$router.push({
-        name: this.createRoute,
-        query: { copyId: this.selectedArticle.id },
-      });
-    },
-    setBarcode(barcode) {
-      this.barcode = barcode ? barcode.code : null;
-    },
-    onArticleClick(article) {
-      this.$router.push({
-        name: this.editRoute,
-        params: {
-          articleId: article.id,
-        },
-      });
-    },
-    handleCurrentChange(row) {
-      this.selectedArticle = row;
-    },
-    avatarClick(article) {
-      this.$router.push({
-        name: this.galleryRoute,
-        params: {
-          articleId: article.id,
-        },
-      });
-    },
-  },
+    })
+    .filter(x => !!x);
+  return {
+    rows: orderBy(rows, 'name'),
+    propColumns: articlePropertySort(propColumns),
+  };
+});
 
-};
+const { t } = useI18n({
+  messages: {
+    en: {
+      copyTip: 'Click to copy "{0}" article',
+    },
+    lt: {
+      copyTip: 'Spustelkite kad nukopijuoti "{0}" prekę',
+    },
+    ru: {
+      copyTip: 'Нажмите, чтобы скопировать номенклатуру "{0}"',
+    },
+  },
+});
+
+function setHeight(height) {
+  tableHeight.value = height;
+}
+
+function downloadSchema() {
+  return {
+    columns: [
+      {
+        key: 'name',
+        title: i18n.global.t('fields.name'),
+        width: 50,
+      }, {
+        key: 'code',
+        title: i18n.global.t('fields.code'),
+        width: 25,
+      },
+      ...tableData.value.propColumns
+        .map(({ id, name }) => ({ key: id, title: name, width: 25 })),
+    ],
+  };
+}
+
+function downloadExcelData() {
+  const schema = downloadSchema();
+  const columns = map(schema.columns, 'key');
+  const data = articles.value.map(a => pick(a, columns));
+  return {
+    schema,
+    data,
+  };
+}
+
+function onAdd() {
+  router.push({
+    name: props.createRoute,
+  });
+}
+
+function onCopy() {
+  router.push({
+    name: this.createRoute,
+    query: { copyId: selectedArticle.value.id },
+  });
+}
+
+function setBarcode(value) {
+  barcode.value = value ? value.code : null;
+}
+
+function onArticleClick(article) {
+  router.push({
+    name: props.editRoute,
+    params: {
+      articleId: article.id,
+    },
+  });
+}
+
+function handleCurrentChange(row) {
+  selectedArticle.value = row;
+}
+
+function avatarClick(article) {
+  router.push({
+    name: props.galleryRoute,
+    params: {
+      articleId: article.id,
+    },
+  });
+}
 
 </script>
 <style scoped lang="scss">
