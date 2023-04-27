109529d802244b1952a30d8d4f6ef1df8410eeee
Alexander Levin <alevin.ru@gmail.com>
2023-04-27 17:43:37 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import log from 'sistemium-debug';
-import Vue from 'vue';
-import { Loading } from 'element-ui';
 import * as Sentry from '@sentry/vue';
 import { BrowserTracing } from '@sentry/tracing';
+import { createApp } from 'vue';
+import { ElLoading } from 'element-plus';
 import { authorizeAxios } from '@/init/HybridDataModel';
 import { authGuard, initData } from '@/services/dataSync';
-import './init';
+import init from './init';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './index.scss';
-import i18n, { saveLocale } from './i18n';
+import i18n from './i18n';
+import { createPinia } from 'pinia';
+// import i18n, { saveLocale } from './i18n';
 
 const { debug, error } = log('main');
-Vue.config.productionTip = false;
+
 router.beforeEach(authGuard);
 
 const { NODE_ENV: environment, VUE_APP_SENTRY_DSN: dsn } = process.env;
@@ -31,32 +33,32 @@ Sentry.init({
   tracesSampleRate: environment === 'production' ? 0.1 : 1,
 });
 
-new Vue({
-  router,
-  store,
-  i18n,
-  watch: {
-    '$i18n.locale': saveLocale,
-  },
-  render: h => h(App),
-  created() {
-    router.onReady(route => {
-      this.$debug('router:ready', route);
-      const loading = Loading.service({});
-      store.dispatch('auth/AUTH_INIT')
-        .then(authorized => {
-          if (authorized) {
-            return store.dispatch('inv/SUBSCRIBE_SOCKET_STATUS');
-          }
-          return loading.close();
-        })
-        .catch(e => {
-          this.$error('auth', e);
-          loading.close();
-        });
+const app = createApp(App)
+  .use(router)
+  .use(i18n)
+  .use(store)
+  .use(createPinia());
+
+init(app);
+app.mount('#app');
+
+// TODO: watch '$i18n.locale': saveLocale
+
+router.isReady().then(() => {
+  debug('router:ready');
+  const loading = ElLoading.service({});
+  store.dispatch('auth/AUTH_INIT')
+    .then(authorized => {
+      if (authorized) {
+        return store.dispatch('inv/SUBSCRIBE_SOCKET_STATUS');
+      }
+      return loading.close();
+    })
+    .catch(e => {
+      error('auth', e);
+      loading.close();
     });
-  },
-}).$mount('#app');
+});
 
 const unsubscribe = store.subscribe(mutation => {
   const { type, payload } = mutation;
@@ -65,11 +67,12 @@ const unsubscribe = store.subscribe(mutation => {
     const { token, account: { name: username } = {} } = payload;
     authorizeAxios(token);
     Sentry.setUser({ username });
+    debug(username);
     store.dispatch('inv/SUBSCRIBE_SOCKET_STATUS')
       .catch(error);
   } else if (type === 'inv/SET_SOCKET_IS_READY' && payload) {
     unsubscribe();
-    const loading = Loading.service({});
+    const loading = ElLoading.service({});
     initData()
       .catch(e => error(e))
       .finally(() => {
