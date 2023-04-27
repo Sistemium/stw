58935e0abb5c07905855568cfe4e1a4199192c02
Alexander Levin <alevin.ru@gmail.com>
2023-04-27 17:10:44 +0300

    Update sistemium.js

--- src/init/sistemium.js
+++ src/init/sistemium.js
@@ -1,14 +1,12 @@
 import Resize from '@/lib/Resize.vue';
 
 import validations from '@/lib/validations';
-import ToolButton from '@/lib/ToolButton.vue';
 import SimpleLabel from '@/lib/SimpleLabel.vue';
 import SelectOnFocus from '@/lib/SelectOnFocus';
 import debug from '@/lib/debug';
 
 // eslint-disable-next-line func-names
 export default function (app) {
-  app.component(ToolButton.name, ToolButton);
   app.component(SimpleLabel.name, SimpleLabel);
   app.component(Resize.name, Resize);
   app.directive('selectOnFocus', SelectOnFocus);
