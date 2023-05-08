b72c27d3dfd880bbd03d724b9b1dd48b8d3a237b
Alexander Levin <alevin.ru@gmail.com>
2023-05-08 21:37:06 +0300

    Update WorkflowButton.vue

--- src/lib/WorkflowButton.vue
+++ src/lib/WorkflowButton.vue
@@ -1,28 +1,40 @@
 <template lang="pug">
 
-el-dropdown.workflow-button(@command="onCommand" :trigger="trigger" v-if="step")
-  el-button(:type="step.style" :size="size" :disabled="disabled") {{ $t(step.label) }}
+el-dropdown.workflow-button(
+  @command="onCommand"
+  :trigger="trigger"
+  v-if="step"
+)
+  el-button(
+    :type="step.style"
+    :size="size"
+    :disabled="disabled"
+  ) {{ $t(step.label) }}
   template(#dropdown)
     el-dropdown-menu
       el-dropdown-item(
-        v-for="option in options" :key="option.to"
+        v-for="option in options"
+        :key="option.to"
         :command="option.to"
       ) {{ $t(option.label) }}
 
 </template>
-<script>
+<script setup lang="ts">
 
-import workflowMixin from '@/lib/workflowMixin';
+import { useWorkflow, workflowProps } from '@/services/workflowing';
 
-export default {
-  name: 'WorkflowButton',
-  props: {
-    trigger: {
-      type: String,
-      default: 'click',
-    },
+const props = defineProps({
+  ...workflowProps,
+  trigger: {
+    type: String,
+    default: 'click',
   },
-  mixins: [workflowMixin],
-};
+});
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string);
+}>();
+
+const { step, options, onCommand } = useWorkflow(props, emit);
 
 </script>
