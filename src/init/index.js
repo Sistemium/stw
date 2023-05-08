a6e8bf00602e60e578d19c7da90768a248a2ac1d
Alexander Levin <alevin.ru@gmail.com>
2023-05-08 15:27:58 +0300

    Update index.js

--- src/init/index.js
+++ src/init/index.js
@@ -1,10 +1,8 @@
-import ScrollTo from 'vue-scrollto';
 import elementUi from './element-ui';
 import sistemium from './sistemium';
 
 // eslint-disable-next-line func-names
 export default function (app) {
   elementUi(app);
-  app.use(ScrollTo);
   sistemium(app);
 }
