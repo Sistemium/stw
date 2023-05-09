8fd7c902b37c37e22dbd426ff1f580d6408e38bb
Alexander Levin <alevin.ru@gmail.com>
2023-05-09 12:10:18 +0300

    Update CounterpartyTypeSwitch.vue

--- src/components/CounterpartyTypeSwitch.vue
+++ src/components/CounterpartyTypeSwitch.vue
@@ -1,32 +1,36 @@
 <template lang="pug">
 
-el-radio-group.counterparty-type-switch(v-model="type" @change="x => $emit('change', x)")
+el-radio-group.counterparty-type-switch(
+  v-model="type"
+  @change="x => emit('change', x || null)"
+)
   el-radio-button(label="LegalEntity") {{ $t('concepts.legalEntity') }}
   //el-radio-button(label="Person") {{ $t('concepts.person') }}
   el-radio-button(label="Storage") {{ $t('concepts.storage') }}
-  el-radio-button(:label="null") {{ $t('concepts.unknown') }}
+  el-radio-button(:label="false") {{ $t('concepts.unknown') }}
 
 </template>
-<script>
+<script setup lang="ts">
 
-export default {
-  name: 'CounterpartyTypeSwitch',
-  props: {
-    value: { type: String, default: null },
+import { computed } from 'vue';
+import type { CounterpartyType } from '@/models/StockOperations';
+
+const props = withDefaults(defineProps<{
+  modelValue?: CounterpartyType;
+}>(), { modelValue: null });
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: CounterpartyType);
+  (e: 'change', value: CounterpartyType);
+}>();
+
+const type = computed({
+  get() {
+    return props.modelValue || false;
   },
-  computed: {
-    type: {
-      get() {
-        return this.value;
-      },
-      set(value) {
-        this.$emit('input', value);
-      },
-    },
+  set(value) {
+    emit('update:modelValue', value || null);
   },
-};
+});
 
 </script>
-<style scoped lang="scss">
-
-</style>
