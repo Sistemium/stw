0bfecde7684ebe1b0fd29e600d4aa534f2c53558
Alexander Levin <alevin.ru@gmail.com>
2023-05-10 14:41:19 +0300

    Update sistemium.js

--- src/init/sistemium.js
+++ src/init/sistemium.js
@@ -1,15 +1,10 @@
-import Resize from '@/lib/Resize.vue';
-
 import validations from '@/lib/validations';
 import SimpleLabel from '@/lib/SimpleLabel.vue';
 import SelectOnFocus from '@/lib/SelectOnFocus';
-import debug from '@/lib/debug';
 
 // eslint-disable-next-line func-names
 export default function (app) {
   app.component('SimpleLabel', SimpleLabel);
-  app.component(Resize.name, Resize);
   app.directive('selectOnFocus', SelectOnFocus);
   app.mixin(validations);
-  app.mixin(debug);
 }
