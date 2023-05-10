bc73adbf51c326eef162d44ee08d081a7ad34dcc
Alexander Levin <alevin.ru@gmail.com>
2023-05-10 07:15:03 +0300

    Update BarcodeFormItem.vue

--- src/components/BarcodeScanner/BarcodeFormItem.vue
+++ src/components/BarcodeScanner/BarcodeFormItem.vue
@@ -1,40 +1,42 @@
 <template lang="pug">
 
-el-form-item.barcode-form-item(:label="label" :prop="prop")
+el-form-item.barcode-form-item(
+  :label="$t('fields.barcode')"
+  :prop="prop"
+)
   el-input(v-model="barcode")
     template(#prepend)
       slot(name="prepend")
     template(#append)
-      el-button(icon="el-icon-close" @click="barcode = ''")
+      el-button(
+        icon="el-icon-close"
+        @click="barcode = ''"
+      )
 
 </template>
-<script>
+<script setup lang="ts">
 
-export default {
-  name: 'BarcodeFormItem',
-  props: {
-    prop: {
-      type: String,
-      default: 'barcode',
-    },
-    value: String,
+import { computed } from 'vue';
+
+const props = defineProps({
+  prop: {
+    type: String,
+    default: 'barcode',
   },
-  computed: {
-    label() {
-      return this.$t('fields.barcode');
-    },
-    barcode: {
-      get() {
-        return this.value;
-      },
-      set(value) {
-        this.$emit('input', value);
-      },
-    },
+  modelValue: String,
+});
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string);
+}>();
+
+const barcode = computed({
+  get() {
+    return props.modelValue;
   },
-};
+  set(value) {
+    emit('update:modelValue', value);
+  },
+});
 
 </script>
-<style scoped lang="scss">
-
-</style>
