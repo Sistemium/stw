792d7a7e4868f6935083fa95f7466fc1cfbfb3be
Alexander Levin <alevin.ru@gmail.com>
2023-05-10 17:39:49 +0300

    rm unused

--- src/lib/drawerEditMixin.js
+++
@@ -1,9 +0,0 @@
-import DrawerEdit from '@/lib/DrawerEdit.vue';
-
-export default {
-  props: {
-    from: Object,
-    isDrawer: { type: Boolean, default: true },
-  },
-  components: { DrawerEdit },
-};
