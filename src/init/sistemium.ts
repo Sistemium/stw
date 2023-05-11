c50e73ff7423e76be2e503128b6a48f442f1d70f
Alexander Levin <alevin.ru@gmail.com>
2023-05-11 11:01:26 +0300

    ts

--- src/init/sistemium.ts
+++ src/init/sistemium.ts
@@ -2,7 +2,6 @@ import validations from '@/lib/validations';
 import SimpleLabel from '@/lib/SimpleLabel.vue';
 import SelectOnFocus from '@/lib/SelectOnFocus';
 
-// eslint-disable-next-line func-names
 export default function (app) {
   app.component('SimpleLabel', SimpleLabel);
   app.directive('selectOnFocus', SelectOnFocus);
