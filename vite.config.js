d6c4bbf689016e72fc6220dd1076f05cf902ab9c
Alexander Levin <alevin.ru@gmail.com>
2023-05-11 15:15:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,81 +1,84 @@
 import vue from '@vitejs/plugin-vue';
 import eslint from 'vite-plugin-eslint';
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
-export default defineConfig({
-  // optimizeDeps: {
-  //   include: ['sistemium-data'],
-  // },
-  plugins: [
-    vue({}),
-    eslint({
-      include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
-    }),
-    legacy({
-      ignoreBrowserslistConfig: true,
-      targets: ['defaults', 'not IE 11'],
-      modernPolyfills: false,
-    }),
-    sentryVitePlugin({
-      org: 'sistemium',
-      project: 'stw',
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-      release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
-      include: './dist',
-      sourcemaps: {
-        assets: "./dist/**",
-      },
-    }),
-  ],
-  define: {
-    'process.env': {},
-    __VUE_I18N_FULL_INSTALL__: true,
-    __VUE_I18N_LEGACY_API__: false,
-    // __INTLIFY_PROD_DEVTOOLS__: false,
-  },
-  build: {
-    // minify: false,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // optimizeDeps: {
+    //   include: ['sistemium-data'],
+    // },
+    plugins: [
+      vue({}),
+      eslint({
+        include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+      }),
+      legacy({
+        ignoreBrowserslistConfig: true,
+        targets: ['defaults', 'not IE 11'],
+        modernPolyfills: false,
+      }),
+      sentryVitePlugin({
+        org: 'sistemium',
+        project: 'stw',
+        authToken: env.SENTRY_AUTH_TOKEN,
+        release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
+        include: './dist',
+        sourcemaps: {
+          assets: './dist/**',
+        },
+      }),
     ],
-  },
-  server: {
-    port: 8890,
-    proxy: {
-      '^/api': {
-        target: 'https://vfsd.sistemium.com',
-        // target: 'http://localhost:3105',
-        changeOrigin: true,
-        // rewrite: path => path.replace(/^\/api/, ''),
+    define: {
+      'process.env': {},
+      __VUE_I18N_FULL_INSTALL__: true,
+      __VUE_I18N_LEGACY_API__: false,
+      // __INTLIFY_PROD_DEVTOOLS__: false,
+    },
+    build: {
+      // minify: false,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-      '/pha': {
-        target: 'http://localhost:3130',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/pha/, ''),
-      },
-      '/xlsx': {
-        // target: 'http://localhost:3300',
-        target: 'https://stw.sistemium.com',
-        changeOrigin: true,
-      },
-      '/ims/': {
-        target: 'https://stw.sistemium.com',
-        changeOrigin: true,
+      extensions: [
+        '.js',
+        '.json',
+        '.jsx',
+        '.mjs',
+        '.ts',
+        '.tsx',
+        '.vue',
+      ],
+    },
+    server: {
+      port: 8890,
+      proxy: {
+        '^/api': {
+          target: 'https://vfsd.sistemium.com',
+          // target: 'http://localhost:3105',
+          changeOrigin: true,
+          // rewrite: path => path.replace(/^\/api/, ''),
+        },
+        '/pha': {
+          target: 'http://localhost:3130',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/pha/, ''),
+        },
+        '/xlsx': {
+          // target: 'http://localhost:3300',
+          target: 'https://stw.sistemium.com',
+          changeOrigin: true,
+        },
+        '/ims/': {
+          target: 'https://stw.sistemium.com',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 });
