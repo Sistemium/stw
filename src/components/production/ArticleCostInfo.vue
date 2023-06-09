5d63838bb9aec3f83f848e947d7ab27c73f8ba47
Alexander Levin <alevin.ru@gmail.com>
2023-06-09 09:13:49 +0300

    ArticleCostInfo with materials

--- src/components/production/ArticleCostInfo.vue
+++ src/components/production/ArticleCostInfo.vue
@@ -14,7 +14,11 @@ el-form-item(prop="cost" :label="$t('fields.cost')")
 
 import { computed, watch } from 'vue';
 import model from '@/models/StockArticleDate.js';
+import filter from 'lodash/filter';
+import uniq from 'lodash/uniq';
+import sumBy from 'lodash/sumBy';
 import type StockArticleDate from '@/models/StockArticleDates';
+import type { MaterialFields } from '@/models/Recipes';
 
 const props = defineProps<{
   storageId: string;
@@ -23,34 +27,67 @@ const props = defineProps<{
   vatRate: number;
   vatPrices: boolean;
   units?: number;
+  materials?: MaterialFields[];
 }>();
 
-const data = computed<StockArticleDate>(() => {
-  const many = model.reactiveManyByIndex('articleId', props.articleId) as StockArticleDate[];
-  return many.filter(stock => {
-    return stock.storageId === props.storageId
-      && stock.date <= props.date
-      && stock.nextDate >= props.date;
-  })[0];
+const data = computed<{ cost: number }>(() => {
+  if (!props.materials) {
+    return stockArticleDateReactive(props.storageId, props.articleId, props.date);
+  }
+  return {
+    cost: sumBy(props.materials, ({ articleId, units }) => {
+      const cost = stockArticleDateReactive(props.storageId, articleId, props.date)?.cost;
+      return cost && units ? units * cost : 0;
+    }),
+  };
 });
 
 const cost = computed(() => {
   return data.value?.cost * (props.vatPrices ? (1 + props.vatRate) : 1);
 });
 
+watch(() => props.materials, async materials => {
+  if (!materials) {
+    return;
+  }
+  const articleIds = filter(uniq(materials.map(({ articleId }) => articleId)));
+  if (!articleIds.length) {
+    return;
+  }
+  await model.find({
+    storageId: props.storageId,
+    articleId: { $in: articleIds },
+    date: { $lte: props.date },
+    nextDate: { $gte: props.date },
+  });
+}, { immediate: true });
+
 watch(() => [props.articleId, props.storageId, props.date].join('|'), async () => {
   const {
     storageId,
     articleId,
     date,
+    materials,
   } = props;
+  if (materials || !storageId || !articleId || !date) {
+    return;
+  }
   await model.find({
     storageId,
     articleId,
     date: { $lte: date },
     nextDate: { $gte: date },
   });
-}, { immediate: true })
+}, { immediate: true });
+
+function stockArticleDateReactive(storageId, articleId, date) {
+  const many = model.reactiveManyByIndex('articleId', articleId) as StockArticleDate[];
+  return many.filter(stock => {
+    return stock.storageId === storageId
+      && stock.date <= date
+      && stock.nextDate >= date;
+  })[0];
+}
 
 </script>
 
