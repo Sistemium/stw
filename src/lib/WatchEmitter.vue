1653a1754f7095979379c45716bf61c14a6891f9
Alexander Levin <alevin.ru@gmail.com>
2023-10-09 16:57:27 +0300

    Create WatchEmitter.vue

---
+++ src/lib/WatchEmitter.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+div(style="display: none")
+</template>
+
+<script setup lang="ts">
+import { watch } from 'vue';
+import debounce from 'lodash/debounce';
+
+const props = defineProps<{
+  value: number;
+}>();
+
+const emit = defineEmits<{
+  (e: 'change', value: number): void;
+}>();
+
+const debounced = debounce(value => {
+  emit('change', value);
+}, 500);
+
+watch(() => props.value, debounced);
+
+emit('change', props.value);
+
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
+</style>
