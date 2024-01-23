2ed6d7d70fef78d7b03448e280e051b64803262f
Alexander Levin <alevin.ru@gmail.com>
2024-01-23 22:04:36 +0200

    StmResize with slot

--- src/lib/StmResize.vue
+++ src/lib/StmResize.vue
@@ -4,7 +4,7 @@
   :style="style"
   ref="root"
 )
-  slot
+  slot(v-bind:resized='resized')
 
 </template>
 <script setup lang="ts">
@@ -18,14 +18,19 @@ const props = defineProps<{
 }>();
 
 const emit = defineEmits<{
-  (e: 'resized', height: number);
+  (e: 'resized', height: number): void;
 }>();
+
+defineSlots<{
+  default(props: { resized: number }): any
+}>()
 
 const root = ref(null);
 const style = ref<{ [key: string]: string | number }>({});
 const top = ref(0);
 const height = ref(0);
 const handleResize = debounce(setHeight, 700);
+const resized = ref<number>();
 
 onMounted(() => {
   window.addEventListener('resize', handleResize);
@@ -39,16 +44,19 @@ onBeforeUnmount(() => {
 })
 
 function setHeight() {
+  if (!root.value) {
+    return
+  }
   const { top: clientTop } = root.value.getBoundingClientRect();
   top.value = clientTop;
   height.value = window.innerHeight;
-  const resized = height.value - top.value - (props.padding || 0);
-  const maxHeight = `${resized}px`;
+  resized.value = height.value - top.value - (props.padding || 0);
+  const maxHeight = `${resized.value}px`;
   style.value = { 'max-height': maxHeight };
   if (props.maximize) {
     style.value = { height: maxHeight };
   }
-  emit('resized', resized);
+  emit('resized', resized.value);
 }
 
 </script>
