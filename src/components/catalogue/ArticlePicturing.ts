42995d9acb455258d1856aee2c4c6ad70dee0666
Alexander Levin <alevin.ru@gmail.com>
2024-01-23 14:31:47 +0200

    ts

--- src/components/catalogue/ArticlePicturing.ts
+++ src/components/catalogue/ArticlePicturing.ts
@@ -1,15 +1,16 @@
 import { saveWithLoading } from '@/lib/validations';
 import ArticleModel from '@/models/Article';
 import Picture from '@/models/Picture';
+import type { IArticle } from '@/models/Articles'
 
-export async function setAvatar(article, picture) {
+export async function setAvatar(article: IArticle, picture: null | { id: string }) {
   await saveWithLoading(() => ArticleModel.updateOne({
     id: article.id,
     avatarPictureId: picture ? picture.id : null,
   }));
 }
 
-export async function removeArticlePicture(article, picture) {
+export async function removeArticlePicture(article: IArticle, picture: { id: string }) {
   await saveWithLoading(async () => {
     await Picture.destroy(picture.id);
     if (article.avatarPictureId === picture.id) {
