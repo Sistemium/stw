656171a02da7382aed5af202eacca26d30239691
Alexander Levin <alevin.ru@gmail.com>
2024-01-25 08:31:22 +0200

    Create PricingSelect.vue

---
+++ src/components/select/PricingSelect.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+el-select.pricing-select(
+  :model-value="pricingId"
+  :clearable="true"
+  @update:model-value="id => { pricingId = id }"
+)
+  el-option(
+    v-for="o in options"
+    :key="o.id"
+    :value="o.id"
+    :label="o.name"
+  )
+</template>
+
+<script setup lang="ts">
+import { computed, watch } from 'vue'
+import Pricing from '@/models/Pricing'
+
+const options = computed(() => Pricing.reactiveFilter())
+const pricingId = defineModel<string>()
+
+watch(options, o => {
+  if (o.length === 1) {
+    pricingId.value = o[0].id
+  }
+}, { immediate: true })
+
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
+</style>
