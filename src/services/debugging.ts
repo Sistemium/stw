4edf082d5d663128ae75c8e2738a2df31e9fde3e
Alexander Levin <alevin.ru@gmail.com>
2024-01-30 12:25:42 +0200

    ts

--- src/services/debugging.ts
+++ src/services/debugging.ts
@@ -1,12 +1,12 @@
-import { getCurrentInstance } from 'vue';
-import { nsLog } from 'sistemium-debug';
+import { getCurrentInstance } from 'vue'
+import { nsLog } from 'sistemium-debug'
 
 type DebugFn = (...args: (string | number | object | boolean)[]) => void;
 
 export function useLog(): { debug: DebugFn, error: DebugFn } {
-  const instance = getCurrentInstance();
-  const ns = `stm:${instance.type.__name}`;
-  const nse = `stm:error:${instance.type.__name}`;
-  const { debug, error } = nsLog(ns, nse);
-  return { debug, error };
+  const instance = getCurrentInstance()
+  const ns = `stm:${instance?.type.__name || 'log'}`
+  const nse = `stm:error:${instance?.type.__name || 'log'}`
+  const { debug, error } = nsLog(ns, nse)
+  return { debug, error }
 }
