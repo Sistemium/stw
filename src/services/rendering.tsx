3e055783f2196f44068b4c530ba87cf7b0e85de9
Alexander Levin <alevin.ru@gmail.com>
2025-03-26 07:11:19 +0200

    Update rendering.tsx

--- src/services/rendering.tsx
+++ src/services/rendering.tsx
@@ -2,7 +2,9 @@ import { td } from '@/lib/validations'
 
 export function renderDate({ cellData }: { cellData: string }) {
   if (!cellData) {
+    // @ts-ignore
     return <span></span>
   }
+  // @ts-ignore
   return <span>{td(cellData, 'short')}</span>
 }
