9479f49cdc903c7623638664acaf36127ae2c40d
Alexander Levin <alevin.ru@gmail.com>
2025-04-17 11:49:24 +0300

    ServiceListInput uniq validation

--- src/components/tasks/ServiceListInput.vue
+++ src/components/tasks/ServiceListInput.vue
@@ -2,15 +2,15 @@
 .service-list-input(
 )
   el-form.service-form(
-    v-for="service in services"
-    :key="service.articleId"
+    v-for="(service, idx) in services"
+    :key="idx"
     :ref="el => addRef(el)"
     :disabled="disabled"
     :rules="rules"
     :model="service"
   )
-    el-link(@click="removeService(service)")
-      small {{ $t('delete') }}
+    el-link(@click="removeService(idx)")
+      small {{ $t('remove') }}
     el-form-item(prop="articleId")
       article-select(
         v-model="service.articleId"
@@ -30,17 +30,27 @@
 
 <script setup lang="ts">
 import { onBeforeUpdate, ref, type Ref } from 'vue'
-import remove from 'lodash/remove'
 import { $requiredRule } from '@/lib/validations'
 import type { ServiceTaskService } from '@/models/ServiceTask'
 import ArticleSelect from '@/components/catalogue/ArticleSelect.vue'
 import PriceForm from '@/components/out/PriceForm.vue'
 import { eachSeries } from 'async'
+import { safeT } from '@/i18n'
 
 const services: Ref<Partial<ServiceTaskService>[]> = defineModel({ default: [] })
 const articleFilter = { isService: true }
-const rules = $requiredRule(['articleId', 'price'])
 const itemRefs = ref([])
+const rules = $requiredRule(['articleId', 'price'])
+
+rules.articleId.push({
+  validator(rule: any, value: any, callback: any) {
+    const count = services.value.filter(({ articleId }) => articleId === value)
+    if (!value || count.length <= 1) {
+      return callback()
+    }
+    callback(new Error(safeT('uniq', 'validation', safeT('fields.service'))))
+  },
+})
 
 function addRef(el: any) {
   if (el) {
@@ -49,8 +59,8 @@ function addRef(el: any) {
 }
 
 onBeforeUpdate(() => {
-  itemRefs.value = [];
-});
+  itemRefs.value = []
+})
 
 defineProps<{
   disabled?: boolean
@@ -67,18 +77,22 @@ type Validator = (result: boolean) => any
 
 defineExpose({
   validate(callback: Validator) {
+    let hasError = false
     eachSeries(itemRefs.value, (item, cb) => {
       item.validate((itemValid: boolean) => {
-        cb(itemValid ? null : Error('invalid item'));
-      });
+        if (!itemValid) {
+          hasError = true
+        }
+        cb(null)
+      })
     }, err => {
-      callback(!err);
-    });
+      callback(!err && !hasError)
+    })
   },
 })
 
-function removeService(service: ServiceTaskService) {
-  remove(services.value, ({ articleId }) => articleId === service.articleId)
+function removeService(idx: number) {
+  services.value.splice(idx, 1)
 }
 
 </script>
