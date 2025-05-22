b3f8eec13a67cfcf29fa030155b6e3138809b41f
Alexander Levin <alevin.ru@gmail.com>
2025-05-22 21:19:06 +0300

    Create SpeechButton.vue

---
+++ src/components/assistant/SpeechButton.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+.speech-button(v-if="isSupported")
+  el-button(
+    @click="toggle"
+    :disabled="busy"
+    :type="isListening ? 'warning' : 'success'"
+    circle
+    :icon="Microphone"
+  )
+</template>
+
+<script setup lang="ts">
+import { useSpeechRecognition } from '@vueuse/core'
+import { watch } from 'vue'
+import { getLocale } from '@/i18n'
+import { Microphone } from '@element-plus/icons-vue'
+
+const { lang = getLocale(), continuous = false } = defineProps<{
+  continuous?: boolean
+  lang?: string
+  busy?: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'spoken', text: string): void
+}>()
+
+const { isSupported, isListening, isFinal, result, start, stop } = useSpeechRecognition({
+  lang,
+  continuous,
+})
+
+function toggle() {
+  if (isListening.value) {
+    stop()
+  } else {
+    start()
+  }
+}
+
+watch(isFinal, is => {
+  if (is) {
+    emit('spoken', result.value)
+  }
+})
+</script>
