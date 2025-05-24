5e42dc7cfe75388c525719f41f8d01e0c012bd42
Alexander Levin <alevin.ru@gmail.com>
2025-05-24 14:29:24 +0300

    applySearch

--- src/services/lt.ts
+++ src/services/lt.ts
@@ -1,6 +1,8 @@
 import each from 'lodash/each';
 import replace from 'lodash/replace';
 import escapeRegExp from 'lodash/escapeRegExp';
+import trim from 'lodash/trim'
+import { BaseItem } from '@/init/Model'
 
 const LT_LETTERS = {
 
@@ -24,4 +26,36 @@ export function likeLt(string: string) {
   });
 
   return new RegExp(res, 'i');
+}
+
+export function searchRe(search: string): { test(s: string): boolean } {
+  const trimmed = trim(search).replace(/ +/g, ' ')
+  if (!trimmed) {
+    return { test: () => true }
+  }
+  const parts = search.split(' ').map(likeLt)
+  if (parts.length === 1) {
+    return likeLt(search)
+  }
+  return { test: s => !parts.find(re => !re.test(s)) }
+}
+
+export function applySearch<T extends BaseItem>(search: string, fields: string[]) {
+  const re = searchRe(search)
+  return (items: T[]) => {
+    if (!search) {
+      return items
+    }
+    return matchRecord(items)
+  }
+
+  function matchRecord(items: BaseItem[]): T[] {
+    return items.filter(item => {
+      return fields.find(field => {
+        const val = item[field] as string | BaseItem[]
+        const isArray = val && Array.isArray(val)
+        return isArray ? matchRecord(val as BaseItem[]).length : re.test(val)
+      })
+    }) as T[]
+  }
 }
