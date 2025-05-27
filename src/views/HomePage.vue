5a3240283cd5fc2f36e1d29340649f05bc1d675b
Alexander Levin <alevin.ru@gmail.com>
2025-05-27 15:17:37 +0300

    AI home

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,18 @@
 <template lang="pug">
 
-.home.text-center
+assistant-page(v-if="store.account")
+.home.text-center(v-else)
   img(alt="Vue logo" src="../assets/logo.png")
   hello-world(:msg="$t('message')")
 
 </template>
 <script setup lang="ts">
 
+import { useInvStore } from '@/store/invStore'
 import HelloWorld from '@/components/HelloWorld.vue';
+import AssistantPage from '@/views/AssistantPage.vue'
+
+const store = useInvStore()
 
 // this.$debug('home');
 </script>
