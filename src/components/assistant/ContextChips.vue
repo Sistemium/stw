0f2522051cf59eca1a28112e5726ea30eb7d41d0
Alexander Levin <alevin.ru@gmail.com>
2025-06-03 22:23:08 +0300

    Create ContextChips.vue

---
+++ src/components/assistant/ContextChips.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+v-chip.mr-2(
+  v-for="item in context"
+  :key="item.id"
+  closable
+  size="small"
+  @click:close="emit('click:close', item.id)"
+) {{ item.label }}
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  disabled?: boolean
+  context?: { id: string, label: string }[]
+}>()
+const emit = defineEmits<{
+  (e: 'click:close', id: string): void
+}>()
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
+</style>
