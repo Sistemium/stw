2c019b72fd9571b9c4fc11150b00d0db00eea7e1
Alexander Levin <alevin.ru@gmail.com>
2025-06-03 22:23:12 +0300

    Create LocalizedStringView.vue

---
+++ src/components/LocalizedStringView.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+span(v-if="localized") {{ localized[getLocale()] }}
+</template>
+
+<script setup lang="ts">
+import type { LocalizedString } from '@/services/prompting'
+import { getLocale } from '@/services/i18n'
+
+defineProps<{
+  localized?: LocalizedString
+}>()
+
+</script>
